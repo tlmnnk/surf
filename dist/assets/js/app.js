e977d1e83b55193a56898a9944bc48652cb70bd6
vladislav t <vt118@mail.ru>
2019-10-15 00:30:36 +0300

    Doing first page

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -3014,6 +3014,16 @@ $(function(){
 
     $('.header__slider').slick({
         infinite: true,
-        fade: true
+        fade: true,
+        asNavFor: '.slider-dots',
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="assets/img/arrow-left.svg">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="assets/img/arrow-right.svg">'
+    });
+
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider'
+
     });
 });
