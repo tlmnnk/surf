1ae1bc15e23577d36b68d4f095d4639af2e35ee9
Vladislav T <vt118@mail.ru>
2019-10-18 15:27:24 +0300

    Doing slider map

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,15 +5,23 @@ $(function(){
     $('.header__slider').slick({
         infinite: true,
         fade: true,
-        asNavFor: '.slider-dots',
+        asNavFor: '.slider-dotshead',
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="assets/img/arrow-left.svg">',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="assets/img/arrow-right.svg">'
     });
 
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider'
+
+    });
+
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="assets/img/arrow-left.svg">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="assets/img/arrow-right.svg">'
 
     });
 });
